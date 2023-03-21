030cf646fd73064fe7b48224f211bf4c78f1da87
AtishnalChand <86387575+AtishnalChand@users.noreply.github.com>
2023-03-21 16:40:16 +1100

    Uploaded bash script for compiling GITM

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module load use.own
+module load GITM
+module load openmpi
+module load netcdf
+module load openmpi hdf5 intel-compiler
+
+./Config.pl -install -compiler=ifortmpif90 -earth
+
+make                                              
