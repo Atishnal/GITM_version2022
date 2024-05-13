d2b7e33ed286c80d0b816564be6127daf14c0973
AtishnalChand <86387575+AtishnalChand@users.noreply.github.com>
2024-05-13 19:06:48 +1000

    Update output_common.f90
    Added IonAverageEnergyFiltered and IonEnergyFluxFiltered in 2DGEL output file.

--- src/output_common.f90
+++ src/output_common.f90
@@ -359,7 +359,7 @@ subroutine output(dir, iBlock, iOutputType)
 
   case ('2DGEL')
 
-     nvars_to_write = 15
+     nvars_to_write = 17
      call output_2dgel(iBlock)
 
   case ('2DMEL')
@@ -623,6 +623,8 @@ contains
        write(iOutputUnit_,"(I7,A1,a)") 13, " ", "FL Length"
        write(iOutputUnit_,"(I7,A1,a)") 14, " ", "Ion_Average_Energy"
        write(iOutputUnit_,"(I7,A1,a)") 15, " ", "Ion_Energy_Flux"
+       write(iOutputUnit_,"(I7,A1,a)") 16, " ", "Ion_Average_Energy_Filtered"
+       write(iOutputUnit_,"(I7,A1,a)") 17, " ", "Ion_Energy_Flux_Filtered"
        
     endif
     
@@ -1647,7 +1649,9 @@ subroutine output_2dgel(iBlock)
              DivJuFieldLine(iLon, iLat), &
              LengthFieldLine(iLon, iLat), &
 	     IonAverageEnergy(iLon,iLat), &
-             IonEnergyFlux(iLon,iLat)
+             IonEnergyFlux(iLon,iLat), &
+	     IonAverageEnergyFiltered(iLon,iLat), &
+             IonEnergyFluxFiltered(iLon,iLat)
      enddo
   enddo
 
